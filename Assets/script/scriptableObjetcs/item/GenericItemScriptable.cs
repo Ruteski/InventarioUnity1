9d29d65c204215fd6757a46dc13c1a954cc2ff4d
Lincoln Ruteski <lruteski@gmail.com>
2024-10-27 15:55:08 -0300

    Inventario
    Criado propriedades das classes dos itens na classe generic - parte 1

--- Assets/script/scriptableObjetcs/item/GenericItemScriptable.cs
+++ Assets/script/scriptableObjetcs/item/GenericItemScriptable.cs
@@ -4,6 +4,38 @@ using UnityEngine;
 
 public abstract class GenericItemScriptable : ScriptableObject
 {
-   
-   
+   #region Declarations
+   [SerializeField]
+   private int id;
+
+   [SerializeField]
+   private Sprite icon;
+
+   [SerializeField]
+   private bool isDroppable;
+
+   [SerializeField]
+   private bool removeWhenNumberisZero;
+
+   [SerializeField]
+   private bool isOnlyItem; // define se o item no inventario var ser unico ou pode ter mais dele ali no slot
+
+   [SerializeField]
+   private string label;
+
+   [SerializeField]
+   [TextArea]
+   private string description;
+
+   [SerializeField]
+   private int currentNumber;
+
+   [SerializeField]
+   private int limitedNumber;
+
+   [SerializeField]
+   private int slotSize = 1;
+
+   #endregion
+
 }
