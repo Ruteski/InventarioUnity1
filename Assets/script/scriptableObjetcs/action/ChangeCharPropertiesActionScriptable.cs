3cc0f40a2eebad152437f6b9c99ac667c36a7a99
Lincoln Ruteski <lruteski@gmail.com>
2024-10-27 18:20:22 -0300

    Actions
    Compreendendo o funcionamento da acao dentro do item

--- Assets/script/scriptableObjetcs/action/ChangeCharPropertiesActionScriptable.cs
+++ Assets/script/scriptableObjetcs/action/ChangeCharPropertiesActionScriptable.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewChangeCharPropertiesAction", menuName = "Action/NewChangeCharPropertiesAction")]
 public class ChangeCharPropertiesActionScriptable : GenericActionScriptable
 {
+   #region Declaration
+
    [SerializeField]
    [Range(-100f, 100f)]
    private float life;
@@ -25,6 +27,13 @@ public class ChangeCharPropertiesActionScriptable : GenericActionScriptable
    private bool applyNewGravity;
 
    [SerializeField]
+   [Range(1f, 4f)]
+   private bool gravity;
+
+   [SerializeField]
+   private bool applyNewPos;
+
+   [SerializeField]
    private Vector3 newPos;
 
    [SerializeField]
@@ -36,11 +45,49 @@ public class ChangeCharPropertiesActionScriptable : GenericActionScriptable
    [SerializeField]
    private bool resetSpeed;
 
+   #endregion
 
+   #region Methods
 
    public override IEnumerator Execute() {
       yield return new WaitForSeconds(DelayToStart);
 
-      // GameController -> ShowPopup
+      if(life != 0) {
+         //GameController => Funcao()
+      }
+
+      if(mana != 0) {
+         //GameController => Funcao()
+      }
+
+      if(speed != 0) {
+         //GameController => Funcao()
+      }
+
+      if(jump != 0) {
+         //GameController => Funcao()
+      }
+
+      if(applyNewGravity) {
+         //GameController => Funcao()
+      }
+
+      if(applyNewPos) {
+         //GameController => Funcao()
+      }
+
+      if(resetJump) {
+         //GameController => Funcao()
+      }
+
+      if(resetGravity) {
+         //GameController => Funcao()
+      }
+
+      if(resetSpeed) {
+         //GameController => Funcao()
+      }
    }
+
+   #endregion
 }
--- Assets/script/scriptableObjetcs/action/PlaySoundActionScriptable.cs
+++ Assets/script/scriptableObjetcs/action/PlaySoundActionScriptable.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewPlaySoundAction", menuName = "Action/NewPlaySoundAction")]
 public class PlaySoundActionScriptable : GenericActionScriptable
 {
+   [SerializeField]
+   private AudioClip audioFile;
+
    public override IEnumerator Execute() {
+
       yield return new WaitForSeconds(DelayToStart);
 
-      // GameController -> ShowPopup
+      if(audioFile != null) {
+         //GameController => Funcao()
+      } else {
+         Debug.LogWarning("There is no audio file in this PlaySound Scriptable Object");
+      }
    }
 }
