6d4302242cba1da65dbf45d75cd8545d8b8ecd53
Lincoln Ruteski <lruteski@gmail.com>
2024-10-27 18:31:29 -0300

    Actions
    Adicionando acoes de equipar itens especificos - armor and weapon

--- Assets/script/scriptableObjetcs/item/ArmorItemScriptable.cs
+++ Assets/script/scriptableObjetcs/item/ArmorItemScriptable.cs
@@ -5,5 +5,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewArmorItem", menuName = "Inventory/items/NewArmorItem")]
 public class ArmorItemScriptable : GenericItemScriptable
 {
-   
+   [SerializeField]
+   private List<GenericActionScriptable> actionEquipList;
+
+   public override void ActionEquipAndUnequipListDispatch() {
+      actionManagerEvent = new ActionManagerEvent();
+      actionManagerEvent.DispatchAllGenericActionListEvent(actionEquipList);
+   }
 }
--- Assets/script/scriptableObjetcs/item/WeaponItemScriptable.cs
+++ Assets/script/scriptableObjetcs/item/WeaponItemScriptable.cs
@@ -5,5 +5,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewWeaponItem", menuName = "Inventory/items/NewWeaponItem")]
 public class WeaponItemScriptable : GenericItemScriptable
 {
+   [SerializeField]
+   private List<GenericActionScriptable> actionEquipList;
 
+   public override void ActionEquipAndUnequipListDispatch() {
+      actionManagerEvent = new ActionManagerEvent();
+      actionManagerEvent.DispatchAllGenericActionListEvent(actionEquipList);
+   }
 }
